2e71cb08d6e5e8f235c40c7c8b73c071acee9c28
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-08 22:54:28 +0000

    correction

--- registration.php
+++ registration.php
@@ -2,11 +2,11 @@
 
 $connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
-$fname = $_POST['fname']
-$username = $_POST['username']
-$password = $_POST['password']
+$fname = $_POST['fname'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-echo($fname, $username, $password)
+echo($fname, $username, $password);
 
 
 ?>

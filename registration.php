30f66dee4bfbf7c5978b03e0904164479fa9c7d5
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-08 22:48:45 +0000

    alternative db connection code

--- registration.php
+++ registration.php
@@ -1,8 +1,8 @@
 <?php
 
-$conn = new mysqli("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
+$connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
-if($conn->connect_error){
+if($connection->connect_error){
     die("connection failed");
 }
 echo "connected successfully";

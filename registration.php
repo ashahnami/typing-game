b44fc53803c65a09e82d7b961082c49e685c3906
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-09 10:16:20 +0000

    correction

--- registration.php
+++ registration.php
@@ -3,7 +3,7 @@
 // establishes connection to database
 $connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
-mysqli_select_db($connection, 'heroku_596c4064b5db331')
+mysqli_select_db($connection, 'heroku_596c4064b5db331');
 
 // retrieves the variables entered in the form
 $first_name = $_POST['fname'];

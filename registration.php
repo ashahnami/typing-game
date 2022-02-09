00a4f61af2deb2a061d65f63bb8b4b7119eb2714
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-09 09:47:30 +0000

    correction

--- registration.php
+++ registration.php
@@ -3,12 +3,12 @@
 // establishes connection to database
 $connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
-$fname = $_POST['fname'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+$first_name = $_POST['fname'];
+$un = $_POST['username'];
+$pw = $_POST['password'];
 
 // selects rows with same username as the entered username
-$s = " SELECT * FROM Users WHERE UserName = '$username'";
+$s = " SELECT * FROM Users WHERE UserName = '$un'";
 
 // checks whether an account with such username already exists
 if($num==1){

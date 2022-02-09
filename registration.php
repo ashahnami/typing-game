935a8aca61caf033ee7c78603096012d2131d55b
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-09 10:04:29 +0000

    correction

--- registration.php
+++ registration.php
@@ -3,6 +3,7 @@
 // establishes connection to database
 $connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
+// retrieves the variables entered in the form
 $first_name = $_POST['fname'];
 $un = $_POST['username'];
 $pw = $_POST['password'];
@@ -11,11 +12,12 @@ $pw = $_POST['password'];
 $s = " SELECT * FROM Users WHERE UserName = '$un'";
 
 // checks whether an account with such username already exists
-if($num==1){
+if($num>0){
     echo "Username already taken";
 }else{
     echo "Account created";
 }
 
 ?>
+
 

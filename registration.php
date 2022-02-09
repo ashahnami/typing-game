a7947584b49859b9f04ecb1f4236ea7fe34683ff
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-09 09:43:12 +0000

    basis of registration.php

--- registration.php
+++ registration.php
@@ -1,12 +1,21 @@
 <?php
 
+// establishes connection to database
 $connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b90a7a1ef0fb35", "6e62399d");
 
 $fname = $_POST['fname'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-echo('test');
+// selects rows with same username as the entered username
+$s = " SELECT * FROM Users WHERE UserName = '$username'";
+
+// checks whether an account with such username already exists
+if($num==1){
+    echo "Username already taken";
+}else{
+    echo "Account created"
+}
 
 ?>
 

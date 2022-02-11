a643cc51ee74d5c26910c23b805e042d7c003ce5
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-11 12:19:21 +0000

    adding user details to database

--- register.php
+++ register.php
@@ -1,32 +1,32 @@
 <?php
-session_start() // starts a session
+session_start(); // starts a session
+    
+require "connection.php"; // includes the connection details
 
-    // includes the connection details
-    include("connection.php");
+if($_SERVER['REQUEST_METHOD']=="POST") // checks whether something has been posted
+{
 
-    // checks whether something has been posted
-    if($_SERVER['REQUEST_METHOD']=="POST")
-    {
+    // retrieves the posted variables
+    $fname = $_POST['fname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        // retrieves the posted variables
-        $fname = $_POST['fname'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    // checks if username is taken
+    $s = "SELECT * FROM users WHERE UserName = '$username'";
+    $result = mysqli_query($connection, $s);
+    $num_of_rows = mysqli_num_rows($result);
 
-        // checks if username is taken
-        $s = "SELECT * FROM users WHERE UserName = '$username'";
-        $result = mysqli_query($connection, $s);
-        $num_of_rows = mysqli_num_rows($result);
+    if($num_of_rows>0){
+        echo "Username already taken. Please retry";
+    }else{
+        $password_hash = hash('sha256',$password); // hashes the password
+        
+        $register = "INSERT INTO users(FirstName,UserName,PasswordHash) VALUES($fname,$username,$password_hash)";
 
-        if($num_of_rows>0){
-            echo "Username already taken. Please retry";
-        }else{
-            echo "Valid username";
-            // adding details to database code goes here
-            
-        }
-
-    }  
+        mysqli_query($connection,$register); // executes the above query 
+        
+    }
+}
 ?>
 
 <!DOCTYPE html>

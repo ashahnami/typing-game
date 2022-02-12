77add1937571feb90c45a885bd4bae958909c66d
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-12 14:34:24 +0000

    correction to signout.php

--- signout.php
+++ signout.php
@@ -2,9 +2,9 @@
 
 session_start(); // starts the session
 
-if(isset($_SESSION['userId'])) // checks whether session variable is  set
+if(isset($_SESSION['userID'])) // checks whether session variable is  set
 {
-	unset($_SESSION['userId']); // unsets the session variable
+	unset($_SESSION['userID']); // unsets the session variable
 
 }
 
@@ -12,3 +12,5 @@ header("Location: login.php"); // redirects to login page
 die;
 
 ?>
+
+

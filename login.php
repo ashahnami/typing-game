b228d3488257c21b877e2ab64274cc4454cba76e
Armin Shahnami <shahnami.armin@gmail.com>
2022-02-12 11:28:18 +0000

    login system

--- login.php
+++ login.php
@@ -1,6 +1,36 @@
 <?php
-    // form is processed here
-?>
+
+session_start(); // starts the session
+
+require "connection.php"; // includes the database connection
+
+if($_SERVER['REQUEST_METHOD'] == "POST") // checks if something has been posted
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM Users WHERE UserName = '$username'"; // selects the rows with same username
+    $result = mysqli_query($connection,$query); // executes the above query
+    
+    if($result && mysqli_num_rows($result) > 0) // checks whether username exists
+    {
+        $hashed_password = hash('sha256',$password); // hashes the entered password
+
+        $userdata = mysqli_fetch_assoc($result); // holds the row in an array form
+
+
+        if($userdata['PasswordHash'] === $hashed_password) // compares entered password with password in database
+        {
+            $_SESSION['userID'] = $userdata['UserID']; // sets the session variable which is unique to the user
+            header("Location: index.php"); // redirects to homepage
+            die;
+        }
+
+    }else{
+        echo "Incorrect login details";
+    }
+
+}
 
 <!DOCTYPE html>
 <html lang="en">
